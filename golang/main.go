bd15ef28df35610917311907441c8dedc5104336
hitto <68594072+hitto-hub@users.noreply.github.com>
2024-04-05 12:49:48 +0900

    golang 難しい、、、

--- golang/main.go
+++ golang/main.go
@@ -1,43 +1,22 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "net/http"
+    // "encoding/json"
+    "database/sql"
+    // "fmt"
+    // "log"
+    // "net/http"
+    // "context"
+    "testing"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/stretchr/testify/assert"
 )
 
-type Article struct {
-    Title   string `json:"Title"`
-    Desc    string `json:"desc"`
-    Content string `json:"content"`
+func TestGetRegisteredDriver(t *testing.T) {
+    assert.Equal(t, []string{"mysql"}, sql.Drivers())
 }
 
-type Articles []Article
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+func main () {
+    t := new(testing.T)
+    TestGetRegisteredDriver(t)
 }
-
-func handleRequests() {
-    http.HandleFunc("/", homePage)
-    http.HandleFunc("/articles", returnAllArticles)
-    log.Fatal(http.ListenAndServe(":8081", nil))
-}
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-    articles := Articles{}
-    for i := 0; i < 10; i++ {
-        title := "Hello_%d"
-        articles = append(
-            articles,
-            Article{Title: fmt.Sprintf(title, i), Desc: "Article Description", Content: "Article Content"})
-    }
-    fmt.Println("Endpoint Hit: returnAllArticles")
-    json.NewEncoder(w).Encode(articles)
-}
-
-func main() {
-    handleRequests()
-}
-
